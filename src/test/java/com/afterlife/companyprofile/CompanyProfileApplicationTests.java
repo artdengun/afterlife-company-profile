b441c137ec31fa9c51f115ee94ba9c1d5b53dd65
artdengun <artdengun@gmail.com>
2023-06-27 17:16:23 +0700

    company profile

---
+++ src/test/java/com/afterlife/companyprofile/CompanyProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afterlife.companyprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
