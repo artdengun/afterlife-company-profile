2f00b6d351af162077fc582b7f6fb6752c5cbdf2
artdengun <artdengun@gmail.com>
2023-09-08 00:12:11 +0700

    done

--- src/main/java/com/afterlife/companyprofile/ApplicationController.java
+++ src/main/java/com/afterlife/companyprofile/ApplicationController.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApplicationController {
 
+    // index html data
     @GetMapping(value = "/index")
     public String getIndex(){
         return "index";
     }
-
-    @GetMapping(value = "/inner-page")
-    public String getInnerPage(){
-        return "inner-page";
-    }
-
-
 }
